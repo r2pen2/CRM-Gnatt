77e92375c9f80643e8ca15e8053906d18d43712b
r2pen2 <joedobbelaar@gmail.com>
2024-06-23 19:03:43 -0400

    prettier updates

--- src/components/LeftColumn/LeftColumn.tsx
+++ src/components/LeftColumn/LeftColumn.tsx
@@ -1,49 +1,12 @@
-import { FC, useState } from "react";
-import { useLanguage } from "@/context/LocaleProvider";
-import Icon from "../Icon";
-import PaginationButton from "../PaginationButton/PaginationButton";
-import { StyledInput, StyledInputWrapper, StyledLeftColumnHeader, StyledWrapper } from "./styles";
+import { FC } from "react";
+import { StyledLeftColumnHeader, StyledWrapper } from "./styles";
 import { LeftColumnProps } from "./types";
 import LeftColumnItem from "./LeftColumnItem/LeftColumnItem";
 
-const LeftColumn: FC<LeftColumnProps> = ({
-  data,
-  rows,
-  onLoadNext,
-  onLoadPrevious,
-  pageNum,
-  pagesAmount,
-  searchInputValue,
-  onSearchInputChange,
-  onItemClick
-}) => {
-  const [isInputFocused, setIsInputFocused] = useState(false);
-  const { search } = useLanguage();
-
-  const toggleFocus = () => setIsInputFocused((prev) => !prev);
-
+const LeftColumn: FC<LeftColumnProps> = ({ data, rows, onItemClick }) => {
   return (
     <StyledWrapper>
-      <StyledLeftColumnHeader>
-        <StyledInputWrapper isFocused={isInputFocused}>
-          <StyledInput
-            placeholder={search}
-            value={searchInputValue}
-            onChange={onSearchInputChange}
-            onFocus={toggleFocus}
-            onBlur={toggleFocus}
-          />
-          <Icon iconName="search" />
-        </StyledInputWrapper>
-        {/* <PaginationButton
-          intent="previous"
-          isVisible={pageNum !== 0}
-          onClick={onLoadPrevious}
-          icon={<Icon iconName="arrowUp" width="16" height="16" />}
-          pageNum={pageNum}
-          pagesAmount={pagesAmount}
-        /> */}
-      </StyledLeftColumnHeader>
+      <StyledLeftColumnHeader /> {/** Literally just whitespace */}
       {data.map((item, index) => (
         <LeftColumnItem
           id={item.id}
@@ -53,14 +16,6 @@ const LeftColumn: FC<LeftColumnProps> = ({
           onItemClick={onItemClick}
         />
       ))}
-      <PaginationButton
-        intent="next"
-        isVisible={pageNum !== pagesAmount - 1}
-        onClick={onLoadNext}
-        icon={<Icon iconName="arrowDown" width="16" height="16" />}
-        pageNum={pageNum}
-        pagesAmount={pagesAmount}
-      />
     </StyledWrapper>
   );
 };
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -11,4 +11,3 @@ export { default as Header } from "./Calendar/Header";
 export { default as Tile } from "./Tiles/Tile";
 export { default as Tiles } from "./Tiles";
 export { default as Tooltip } from "./Tooltip";
-export { default as PaginationButton } from "./PaginationButton";
